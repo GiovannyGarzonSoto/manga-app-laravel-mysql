73e82d23ac918a3735b214589e92f13e6b450b53
GiovannyGarzonSoto <giovankyandres@gmail.com>
2023-08-22 00:45:34 -0400

    init project && Author crud

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Author;
+use Illuminate\Http\Request;
+
+class AuthorController extends Controller
+{
+    public function index()
+    {
+        $authors = Author::all();
+        if (!$authors) {
+            return response()->json([
+                'success' => false,
+                'message' => 'No se pudieron listar los Autores'
+            ]);
+        }
+        return response()->json([
+            'success' => true,
+            'data' => $authors
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $author = new Author;
+        $author->name = $request->name;
+        $author->save();
+        if (!$author) {
+            return response()->json([
+                'success' => false,
+                'message' => 'No se pudo guardar el Autor'
+            ]);
+        }
+        return response()->json([
+            'success' => true,
+            'data' => $author
+        ]);
+    }
+
+    public function showOne(Author $author)
+    {
+        //
+    }
+
+    public function update(Request $request, Author $author)
+    {
+        if (!$author) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Autor no existe'
+            ]);
+        }
+    
+        $author->update([
+            'name' => $request->input('name')
+        ]);
+    
+        return response()->json([
+            'message' => 'Autor actualizado correctamente',
+            'data' => $author
+        ]);
+    }
+
+    public function destroy(Author $author)
+    {
+        $author->delete();
+        if (!$author) {
+            return response()->json([
+                'success' => false,
+                'message' => 'No se pudo eliminar el Autor'
+            ]);
+        }
+        return response()->json([
+            'message' => 'Autor eliminado correctament',
+            'data' => $author
+        ]);
+    }
+}
